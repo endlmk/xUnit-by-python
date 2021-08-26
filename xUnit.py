4fbb7be7af202a07880ef63c214f8ef4180804fa
endlmk <1269467+endlmk@users.noreply.github.com>
2021-08-26 23:06:39 +0900

    Done setUp.

--- xUnit.py
+++ xUnit.py
@@ -2,26 +2,36 @@
 class TestCase:
     def __init__(self, name) -> None:
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
-        self.wasRun = None
         TestCase.__init__(self, name)
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
         
 # test code
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test =WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetup(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetup").run()
 
 # test = WasRun("testMethod")
 # print(test.wasRun)
