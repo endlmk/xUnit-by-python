d7b684957ad0429f4454dcf490dcf9982dd2f4be
endlmk <1269467+endlmk@users.noreply.github.com>
2021-08-31 23:15:43 +0900

    Impl report failed test.

--- xUnit.py
+++ xUnit.py
@@ -8,8 +8,11 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
         return result
     def tearDown(self):
@@ -18,10 +21,13 @@ class TestCase:
 class TestResult:
     def __init__(self) -> None:
         self.runCount = 0
+        self.errorCount = 0
     def testStarted(self):
         self.runCount = self.runCount + 1
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
