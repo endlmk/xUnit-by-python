4a37eea58d2d70fb1971dfc7fe311c4a84260a81
endlmk <1269467+endlmk@users.noreply.github.com>
2021-08-31 23:10:58 +0900

    Impl report result.

--- xUnit.py
+++ xUnit.py
@@ -5,12 +5,23 @@ class TestCase:
     def setUp(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
     def tearDown(self):
         pass
+
+class TestResult:
+    def __init__(self) -> None:
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
@@ -20,16 +31,28 @@ class WasRun(TestCase):
     def testMethod(self):
         self.log = self.log + "testMethod "
     def tearDown(self):
-        self.log = self.log + "tearDown "        
+        self.log = self.log + "tearDown "
+    def testBrokenMethod(self):
+        raise Exception   
 # test code
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
 
 # test = WasRun("testMethod")
 # print(test.wasRun)
